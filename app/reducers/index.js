623bf1abf020134fcd153e90a1253798867544c1
Aleksandr Basalilov <aleks@partsdetect.com>
2016-11-11 14:57:19 -0800

    Compile reducers and actions

---
+++ app/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+//put new reducers here like this:
+//  reducerKey: reducerName 
+});
+
+export default rootReducer;
