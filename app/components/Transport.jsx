7eda834f11f73c4c5fbb5bf9ceda7cf9f7f5e0e7
clarkdowner <cddowner@gmail.com>
2016-11-11 17:26:48 -0800

    Create play button in transport component

--- app/components/Transport.jsx
+++ app/components/Transport.jsx
@@ -7,14 +7,18 @@ import {connect} from 'react-redux';
 class Transport extends React.Component {
   constructor(props) {
     super(props);
+    this.transportPlay = this.transportPlay.bind(this);
   }
   componentDidMount() {
 
   }
+  transportPlay() {
+    this.props.dispatch({type: 'TIME_ZERO'});
+  }
   render() {
     return (
-      <div id="transport">
-        
+      <div id="transport" style={{height: '1em'}}>
+        <i className="fa fa-play" aria-hidden="true" onClick={this.transportPlay}></i>
       </div>
     );
   }
