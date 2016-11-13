f118a1b36d3661a202e360065b2c819e0d7bfe9e
Aleksandr Basalilov <abasalilov@gmail.com>
2016-11-12 16:28:23 -0800

    Add Knob Components to Sampler Component
    * Add knob
    
    * hotfix for css

--- app/components/Volume.jsx
+++ app/components/Volume.jsx
@@ -11,11 +11,24 @@ class Volume extends React.Component {
   }
   render() {
     return (
-    <div > 
-      <svg>
-        <circle cx={50} cy={50} r={10} fill="red" />
-      </svg>      
-    </div>
+      <div className="samplerVol"> 
+        <svg viewBox="-6 -6 12 12" className="dial">
+          <defs>
+            <radialGradient id="knobgradient">
+              <stop offset="0" stopColor="yellow"/>
+              <stop offset="1" stopColor="silver"/>
+            </radialGradient>
+          </defs>
+      <g className="knob">
+        <circle className="knob_center" cx="0" cy="0" r="0.015625"/>
+          <g className="knob_gfx">
+            <circle cx="0" cy="0" r="5"/>
+              <line x1="0" y1="-2.5" x2="0" y2="-4.5"/>
+          </g>
+      <text className="knob_number"/>
+      </g>
+      </svg>    
+      </div>
       );
   }
 }
