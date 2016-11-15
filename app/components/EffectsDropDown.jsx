9394467431a2a39741c93fce46a9a00901ca0b48
clarkdowner <cddowner@gmail.com>
2016-11-14 20:19:21 -0800

    Add button and structure for new effect creation

---
+++ app/components/EffectsDropDown.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styles from '../App.scss';
+
+import {connect} from 'react-redux';
+
+class EffectsDropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.addEffectToRack = this.addEffectToRack.bind(this);
+  }
+
+  toggleMenu() {
+    this.props.dispatch({type: 'EFFECT_MENU_TOGGLE'});
+  }
+
+  addEffectToRack(e) {
+    this.props.dispatch({type: 'EFFECT_TO_RACK', effect: e.target.value});
+  }
+
+  render() {
+
+    const style = {
+      fontSize: '2em',
+      height: '100%',
+      padding: '.05em .1em 0 .1em',
+      border: '1px solid blue',
+      display: 'block',
+      height: 'auto',
+    };
+    const dropDownBorder = {
+      border: '1px solid green',
+    };
+    const listBorder = {
+      border: '1px solid orange',
+    }
+    const listItemStyle = {
+      display: 'block',
+      height: 'auto',
+    }
+
+    let DropDownItems;
+    let ButtonDisplay;
+    let customEffects = this.props.customEffects;
+    if (this.props.effectsMenuActive) {
+      ButtonDisplay = 'fa fa-minus-square-o';
+      DropDownItems = customEffects.map((customEffect) => {
+        return <li key={customEffects.indexOf(customEffect)} value={customEffect} style={listItemStyle} onClick={this.addEffectToRack}>{customEffect}</li>
+      });
+    } else {
+      ButtonDisplay = 'fa fa-plus-square-o';
+      DropDownItems = '';
+    }
+
+    return (
+      <div className="effectsDropDown" style={dropDownBorder}>
+          <i className={ButtonDisplay} aria-hidden="true" style={style} onClick={this.toggleMenu}></i>
+          <ul style={listBorder}>{DropDownItems}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = function(state) {
+  return {
+    effectsMenuActive: state.effectsMenuActive,
+    customEffects: state.customEffects,
+  };
+}
+
+export default connect(mapStateToProps)(EffectsDropDown);
--- app/components/Player.jsx
+++ app/components/Player.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../App.scss';
 import {connect} from 'react-redux';
 import Transport from './Transport.jsx';
+import EffectsRack from './EffectsRack.jsx';
 import Sampler from './Sampler.jsx';
 import Synth from './Synth.jsx';
 import Fader from './Fader.jsx';
@@ -16,7 +17,7 @@ class Player extends React.Component {
         <div style={{width: '100%', height: '4em'}}></div>
         <Transport />
         <Sampler />
-        <div className="fxrack-placeholder" style={{border: '2px solid red', width: '100%', height:'15%', position:'relative'}}></div>
+        <EffectsRack />
         <Synth />
       </div>
     );
