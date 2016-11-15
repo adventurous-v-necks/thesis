52cf73d390725a4da7801ca8bcbf8da5252b014e
Aleksandr Basalilov <aleks@partsdetect.com>
2016-11-14 17:00:21 -0800

    signIn

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ import Player from './components/Player.jsx';
 import FourOhFour from './components/FourOhFour.jsx';
 import LoginForm from './components/LoginForm.jsx';
 import Landing from './components/Landing.jsx';
-
+import SignIn from './components/SignIn.jsx'
 import reducer from './reducer.js';
 import Sampler from './components/Sampler.jsx'
 import {createStore} from 'redux';
@@ -25,6 +25,7 @@ render(
           <Route path="/" component={Landing} />
           <Route path="/player" component={Player} /> {/* haven't changed anything in server.js yet */}
           <Route path="/sampler" component={Sampler} />
+          <Route path="/signin" component={SignIn} />
           <Route path="tryLogin(/:failed)" component={LoginForm} />
           <Route path="*" component={FourOhFour}/>
       </Route>
