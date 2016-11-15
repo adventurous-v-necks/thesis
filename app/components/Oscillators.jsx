10dfe1473f35f2992735e4a9bf9c55157e0af371
Hans Trautlein <hans@trautlein.com>
2016-11-14 16:16:07 -0800

    Oscillators component and subcomponents first created

---
+++ app/components/Oscillators.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from '../App.scss';
+import {connect} from 'react-redux';
+import Oscillator from './Oscillator.jsx'
+
+class Oscillators extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const style = {
+      height: '45%',
+      width: '50%',
+      border: '3px solid pink',
+      position: 'absolute',
+      left: '2%',
+    }
+
+    const threeOsc = [1, 2, 3].map(number => {
+      return <Oscillator key={number} number={number}/>
+    });
+
+    return (
+      <div className="oscillators" style={style}>
+        {threeOsc}
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = function(state) {
+  return {};
+}
+
+export default connect(mapStateToProps)(Oscillators);
+
