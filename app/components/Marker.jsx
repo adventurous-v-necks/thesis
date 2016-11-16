4b17dc782ad852182df010859d4124b44211f2ed
Hans Trautlein <hans@trautlein.com>
2016-11-16 12:09:18 -0800

    Marker starts when recording button is first hit
    Still have some bugs, e.g., when you try to pause the recording the
    marker will continue to increment. Must add something to state to fix
    that.

--- app/components/Marker.jsx
+++ app/components/Marker.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../App.scss';
-
 import {connect} from 'react-redux';
 
 class Marker extends React.Component {
@@ -8,21 +7,24 @@ class Marker extends React.Component {
     super(props);
   }
 
-  formatTime = time => {
+  formatTime = (time) => {
     // assume time comes in in seconds
-    const leadingSecZero = (t) => {
-      const floor = Math.floor(t % 60).toString();
-      if (t % 60 >= 10) {
+    const leadingZero = (t, seconds) => {
+      const floor = Math.floor(seconds ? t % 60 : t / 60).toString();
+      if (seconds ? t % 60 >= 10 : t / 60 >= 10) {
         return floor;
       } else {
         return '0' + floor;
       }
     }
 
-    const seconds = leadingSecZero(time);
-    const minutes = Math.floor(time / 60).toString();
+    const seconds = leadingZero(time, true);
+    // if we don't want leading zero reinstate lower line
+    // const minutes = Math.floor(time / 60).toString();
+    const minutes = leadingZero(time, false);
+    const tenths = Math.round(time * 10).toString().slice(-1);
     
-    const shortTime = minutes + ':' + seconds;
+    const shortTime = minutes + ':' + seconds + '.' + tenths;
 
     if (time > 3600) {
       const hours = Math.floor(time / (60 * 60)).toString();
@@ -32,20 +34,39 @@ class Marker extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.timeMarker = setInterval( () => {
+      this.props.dispatch({type: 'MARKER_UPDATE'});
+    }, 50);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeMarker);
+  }
+
   render() {
+    const getTime = () => {
+      let time;
+      if (this.props.recordTimeZero) {
+        time = this.props.timeZero - this.props.recordTimeZero;
+      } else {
+        time = 0
+      }
+      return this.formatTime(time);
+    };
     const style = {
       height: '100%',
       width: '20%',
       textAlign: 'center',
       top: '50%',
       transform: 'translateY(-50%)',
+      fontFamily: '"Lucida Console", Monaco, monospace',
+      fontSize: '150%',
     };
-
-    console.log(this.props);
 
     return (
       <div className="marker" style={style}>
-        {this.props.timeZero ? this.formatTime(this.props.timeZero) : '0:00'}
+        {getTime()}
       </div>
     );
   }
@@ -54,7 +75,8 @@ class Marker extends React.Component {
 
 const mapStateToProps = function(state) {
   return {
-    timeZero: state.timeZero
+    timeZero: state.timeZero,
+    recordTimeZero: state.recordTimeZero,
   };
 }
 
