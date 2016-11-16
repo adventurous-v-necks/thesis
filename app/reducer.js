8b0bfc275dd8f431d5f5c553c4d12a805528cf84
Tom Grek <tom.grek@gmail.com>
2016-11-16 15:16:11 -0800

    Implement independent column volume changes

--- app/reducer.js
+++ app/reducer.js
@@ -215,6 +215,7 @@ export default function reduce(state, action) {
       }
       if (action.id >= 1 && action.id <=4) { // one of the column volume knobs
         for (var sample of state.samples[action.id-1]) {
+          console.log(sample);
           if (sample.playing) sample.gainNode.gain.value = action.value / 100;
         }
       }
