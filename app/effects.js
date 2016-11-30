65e0d951d57c045b06290ed8ec1eba5d8ab5681f
clarkdowner <cddowner@gmail.com>
2016-11-29 16:13:02 -0800

    MOOG functional with both cutoff and resonance dials

--- app/effects.js
+++ app/effects.js
@@ -26,33 +26,32 @@ export let BiquadFilterHi = (audioCtx) => {
 
 export let MOOGFilter = (audioCtx) => {
   let bufferSize = 4096;
-  // let effect = (function() {
-    let filter = audioCtx.createScriptProcessor(bufferSize, 1, 1);
-    let in1, in2, in3, in4, out1, out2, out3, out4;
-    in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0;
-    filter.cutoff = 0.065; // between 0.0 and 1.0
-    filter.resonance = 3.99; // between 0.0 and 4.0
-    filter.onaudioprocess = function(e) {
-      let input = e.inputBuffer.getChannelData(0);
-      let output = e.outputBuffer.getChannelData(0);
-      let f = filter.cutoff * 1.16;
-      let fb = filter.resonance * (1.0 - 0.15 * f * f);
-      for (let i = 0; i < bufferSize; i++) {
-        input[i] -= out4 * fb;
-        input[i] *= 0.35013 * (f*f)*(f*f);
-        out1 = input[i] + 0.3 * in1 + (1 - f) * out1; // Pole 1
-        in1 = input[i];
-        out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
-        in2 = out1;
-        out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
-        in3 = out2;
-        out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
-        in4 = out3;
-        output[i] = out4;
-      }
+  console.log('inside effects MOOGFilter')
+  let filter = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+  let in1, in2, in3, in4, out1, out2, out3, out4;
+  in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0;
+  filter.cutoff = 0.065 * 2; // between 0.0 and 1.0
+  filter.resonance = 6 * 100 / 256 //3.99; // between 0.0 and 4.0
+  filter.onaudioprocess = function(e) {
+    let input = e.inputBuffer.getChannelData(0);
+    let output = e.outputBuffer.getChannelData(0);
+    let f = filter.cutoff * 1.16;
+    let fb = filter.resonance * (1.0 - 0.15 * f * f);
+    for (let i = 0; i < bufferSize; i++) {
+      input[i] -= out4 * fb;
+      input[i] *= 0.35013 * (f*f)*(f*f);
+      out1 = input[i] + 0.3 * in1 + (1 - f) * out1; // Pole 1
+      in1 = input[i];
+      out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
+      in2 = out1;
+      out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
+      in3 = out2;
+      out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
+      in4 = out3;
+      output[i] = out4;
     }
-    return filter;
-  // })();
+  }
+  return filter;
 }
 
 export let Distortion = (audioCtx) => {
